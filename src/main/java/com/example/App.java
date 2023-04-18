bca14b5f804caed6b005a0c0f53cd91b0928917f
Javier <javi.ssweb@gmail.com>
2023-04-18 19:23:35 +0200

    Iniciando proyecto

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringWebMercadoSanchiApplication {
+public class App {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringWebMercadoSanchiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
 
 }
