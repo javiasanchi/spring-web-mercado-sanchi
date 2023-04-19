62140796204b2ae03f7942d1fedbd7bc348abd75
javiasanchi <122261431+javiasanchi@users.noreply.github.com>
2023-04-19 13:04:34 +0200

    Html productos funcional columna Acciones

--- src/main/java/com/example/controller/ProductosController.java
+++ src/main/java/com/example/controller/ProductosController.java
@@ -1,10 +1,15 @@
 package com.example.controller;
 
+import com.example.entities.Productos;
 import com.example.service.ProductosService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.Optional;
 
 @AllArgsConstructor
 @Controller
@@ -14,8 +19,20 @@ public class ProductosController {
 
     @GetMapping("productos")
     public String findAll(Model model) {
+        List<Productos> productos = productosServ.findAll();
+        model.addAttribute("productos", productos);
         model.addAttribute("mensaje", "Listado de productos");
-        model.addAttribute("productos", productosServ.findAll());
         return "productos";
+    }
+
+    @GetMapping("productos/{id}")
+    public String findById(Model model, @PathVariable Long id) {
+        Optional<Productos> productOpt = productosServ.findById(id);
+        if (productOpt.isPresent())
+            model.addAttribute("producto", productOpt.get());
+        else
+            model.addAttribute("error", "PRODUCTO NO SE ENCUENTRA O NO EXISTE");
+        return "info-producto";
+
     }
 }
